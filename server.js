8ff272463a980f27901171a3e20d676adfebf7bc
mrharshansarvaiya21 <98145008+mrharshansarvaiya21@users.noreply.github.com>
2022-01-21 13:04:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/tech-blog'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/tech-blog/index.html'));});
-app.listen(process.env.PORT || 8080);
+const app = express();
+app.use(requireHTTPS);
+app.use(express.static('./dist/tech-blog'));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/tech-blog/'}
+);
+});
